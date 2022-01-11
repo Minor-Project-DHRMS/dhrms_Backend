38a6ca752167779436623b5396f997f2ebc831b4
Afzal Ahmed Pinjar <68552769+afzalap@users.noreply.github.com>
2022-01-11 18:11:32 +0530

    refactoring

--- contracts/Doctor.sol
+++ contracts/Doctor.sol
@@ -4,23 +4,24 @@ pragma solidity ^0.8.0;
 
 contract Doctor{
 
-    string private name;
+    string private doctorName;
     address private DID;
-    string private phno;
+    string private phoneNumber;
     string private qualification;
     string private photo;
     string private dob;
     address private HID;
     mapping(address => bool) private patients;
-    address[] PIDList;
+    address[] patientlist;
     string private department;
     uint patientCount;
+
     //  "Raja","2342342342","MBBS","asdsdfsdf","12/02/2123",0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db,0x0A098Eda01Ce92ff4A4CCb7A4fFFb5A43EBC70DC,"Medical"
-    constructor(string memory _name,string memory _phno,string memory _qualification,string memory _photo,string memory _dob,address _HID,address _DID,string memory _department)
-     {
-        name =_name;
+    constructor(string memory _doctorNstring memory _phoneNumber,string memory _qualification,string memory _photo,string memory _dob,address _HID,address _DID,string memory _department)
+    {
+        doctorN =_doctorN
         DID = _DID;
-        phno = _phno;
+        phoneNumber = _phoneNumber;
         qualification = _qualification;
         photo = _photo;
         dob = _dob;
@@ -28,12 +29,12 @@ contract Doctor{
         department = _department;
     }
 
-    function getDoctorName() public view returns(string memory){
-        return name;
+    function getDoctordoctorN) public view returns(string memory){
+        return doctorN
     }
 
-    function setDoctorName(string memory _name) public {
-        name = _name;
+    function setDoctordoctorNstring memory _doctorN public {
+        doctorN= _doctorN
     }
 
     function getDID() public view returns(address){
@@ -44,12 +45,12 @@ contract Doctor{
         DID = _DID;
     }
 
-    function setPhNO(string memory _phno) public {
-        phno = _phno;
+    function setphoneNumber(string memory _phoneNumber) public {
+        phoneNumber = _phoneNumber;
     }
 
-    function getPhNo() public view returns(string memory){
-        return phno;
+    function getphoneNumber() public view returns(string memory){
+        return phoneNumber;
     }
 
     function setQualification(string memory _qualification) public {
@@ -78,23 +79,22 @@ contract Doctor{
 
     function addPatient(address _PID) public{
         patients[_PID] = true;
-        PIDList.push(_PID);
+        patientlist.push(_PID);
     }
 
     function removePatient(address _PID) public {
-       uint i;
        delete patients[_PID];
    
-       for(i=0;i<PIDList.length;i++){
-           if(PIDList[i] == _PID){
+       for(unit i=0; i < patientlist.length; i++){
+           if(patientlist[i] == _PID){
                break;
            }
        }
-       delete PIDList[i];
+       delete patientlist[i];
     }
 
     function getPatientList() public view returns(address[] memory){
-        return PIDList;
+        return patientlist;
     }
 
 
