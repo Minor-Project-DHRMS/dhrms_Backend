0ba7ed06c5a9410ceb455eb073d169209c056007
Nandeesh B K <70832915+Nandeesh68@users.noreply.github.com>
2022-05-06 10:16:35 +0530

    Added credentials

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
-require('solidity-coverage')
+require("solidity-coverage");
+require("dotenv").config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -25,7 +26,13 @@ module.exports = {
         enabled: true,
         runs: 1,
       },
-      evmVersion: "byzantium"
+      evmVersion: "byzantium",
+    },
+  },
+  networks: {
+    rinkeby: {
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
--- scripts/run.js
+++ scripts/run.js
@@ -1,12 +1,47 @@
 const main = async () => {
-  const mainContractFactory = await hre.ethers.getContractFactory('Main');
+  const mainContractFactory = await hre.ethers.getContractFactory("Main");
   const [deployer] = await hre.ethers.getSigners();
-  const mainContract = await mainContractFactory.deploy("Dharwad-office", "7975578890");
+  const mainContract = await mainContractFactory.deploy(
+    "Dharwad-office",
+    "7975578890"
+  );
+
+  var pd = {
+    details: {
+      name: "Patient",
+      DOB: "19-12-2020",
+      gender: "male",
+      height: 19,
+      weight: 20,
+      bloodGroup: "B+ve",
+      email: "patient@gmail.com",
+      phone: 12345678,
+      address: "abc",
+      Ename: "afzal",
+      EPhone: 7975578890,
+      drugAllergies: ["a", "b"],
+      otherIllness: ["pleg"],
+      anyOperations: ["none"],
+      anySurgery: ["dust"],
+      currentMedications: ["aaaa"],
+      healthyAndUnhealthyHabits: ["3 cup coffe perday"],
+      dietType: "roti",
+      caffeineConsumption: 4,
+      smokingPerDay: "none",
+      otherMedicalConditions: "asd",
+    },
+  };
+
   await mainContract.deployed();
 
-  console.log('Contract add:', mainContract.address);
+  await mainContract.addGovernmentOffice(
+    "Hubli-office",
+    "9433387654",
+    "0x8B8979B6580F1c0797c6B9AFE6cAA22DF8417460"
+  );
 
-
+  console.log("Contract add:", mainContract.address);
+  console.log("Deployer add:", deployer.address);
 
   // /*
   //  * Let's try two waves now
@@ -26,38 +61,37 @@ const main = async () => {
   // let allWaves = await waveContract.getAllWaves();
   // console.log(allWaves);
 
-  const pd = {
-    "details": {
-      "name": "Sahil",
-      "DOB": "19-12-2020",
-      "gender": "male",
-      "height": 19,
-      "weight": 20,
-      "bloodGroup": "o",
-      "email": "ashjjsjk.gmail.com",
-      "phone": 12345678,
-      "address": "abc",
-      "Ename": "afzal",
-      "EPhone": 123456,
-      "drugAllergies": ["a", "b"],
-      "otherIllness": ["pleg"],
-      "anyOperations": ["none"],
-      "anySurgery": ["dust"],
-      "currentMedications": ["aaaa"],
-      "healthyAndUnhealthyHabits": ["3 cup coffe perday"],
-      "dietType": "roti",
-      "caffeineConsumption": 4,
-      "smokingPerDay": "none",
-      "otherMedicalConditions": "asd"
-    }
+  const pdh = {
+    details: {
+      name: "Sahil",
+      DOB: "19-12-2020",
+      gender: "male",
+      height: 19,
+      weight: 20,
+      bloodGroup: "o",
+      email: "ashjjsjk.gmail.com",
+      phone: 12345678,
+      address: "abc",
+      Ename: "afzal",
+      EPhone: 123456,
+      drugAllergies: ["a", "b"],
+      otherIllness: ["pleg"],
+      anyOperations: ["none"],
+      anySurgery: ["dust"],
+      currentMedications: ["aaaa"],
+      healthyAndUnhealthyHabits: ["3 cup coffe perday"],
+      dietType: "roti",
+      caffeineConsumption: 4,
+      smokingPerDay: "none",
+      otherMedicalConditions: "asd",
+    },
   };
 
-
-  const addPtxn = await mainContract.addPatient(JSON.stringify(pd), "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4");
+  const addPtxn = await mainContract.addPatient(
+    JSON.stringify(pdh),
+    "0xEF6Bf9C3b802950F6290aF19d85443B1984b4369"
+  );
   await addPtxn.wait();
-
-  const getGovD = await mainContract.getGovernmentDetails(deployer.address);
-  console.log(getGovD);
 
   // const addGtxn = await mainContract.addGovernmentOffice("Dharwad-Office","2343423424","0x5B38Da6a701c568545dCfcB03FcB875f56beddC4");
   // await addGtxn.wait();
@@ -68,14 +102,15 @@ const main = async () => {
   // const addDtxn = await mainContract.addDoctor("ABCD", "2343214234","MBBS","photo","1/3/1999","0x5B38Da6a701c568545dCfcB03FcB875f56b00000eddC4","DGGGGG");
   // await addDtxn.wait();
 
+  // const getDtxn = await mainContract.getPatientDetails(
+  //   "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4"
+  // );
+  // console.log(getDtxn);
 
-
-  const getDtxn = await mainContract.getPatientDetails("0x5B38Da6a701c568545dCfcB03FcB875f56beddC4");
-  console.log(getDtxn);
-
-  const isGov = await mainContract.isGovernment("0x5B38Da6a701c568545dCfcB03FcB875f56beddC4");
-  console.log(isGov);
-
+  // const isGov = await mainContract.isGovernment(
+  //   "0x5B38Da6a701c568545dCfcB03FcB875f56beddC4"
+  // );
+  // console.log(isGov);
 };
 
 const runMain = async () => {
